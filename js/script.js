7eb95ec7b4ae4d71b918d0fca65c513e30714148
Sagarmahesh <sagarmahesh7024@gmail.com>
2024-10-15 22:03:24 +0530

    Made Website responsive for mobile users

--- js/script.js
+++ js/script.js
@@ -6,12 +6,3 @@ const nav = document.querySelector('nav');
 menuToggle.addEventListener('click', () => {
     nav.classList.toggle('active');
 });
-
-
-// dark and light mode switch
-
-const toggleSwitch = document.getElementById('dark-mode-toggle');
-
-toggleSwitch.addEventListener('change', () => {
-    document.body.classList.toggle('dark-mode');
-});
